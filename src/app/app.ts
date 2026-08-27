bbdadb429dc93e4ff511fcb8e35d421160b7f2d5
otto-menza <otoniel.jump@gmail.com>
2026-08-27 17:17:18 -0500

    header/menu configured

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
+import { Cabecero } from './components/cabecero/cabecero';
 import { RouterOutlet } from '@angular/router';
+import { PieDePagina } from './components/pie-de-pagina/pie-de-pagina';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Cabecero, PieDePagina],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/cabecero/cabecero.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-cabecero',
+  imports: [RouterModule],
+  templateUrl: './cabecero.html',
+  styleUrl: './cabecero.css',
+})
+export class Cabecero {
+
+}
