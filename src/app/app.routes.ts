ea808ef07d6fd982f741ae6935ccc1bc06995322
otto-menza <otoniel.jump@gmail.com>
2026-08-27 15:20:30 -0500

    Create components and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ListadoContactos } from './components/listado-contactos/listado-contactos';
+import { Formulario } from './components/formulario/formulario';
+import { Error404 } from './components/error/error';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: ListadoContactos},
+    {path:'listado', component: ListadoContactos},
+    {path:'agregar', component: Formulario},
+    {path:'editar/:llave', component: Formulario},
+    {path:'**', component: Error404}
+];
---
+++ src/app/components/listado-contactos/listado-contactos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-contactos',
+  imports: [],
+  templateUrl: './listado-contactos.html',
+  styleUrl: './listado-contactos.css',
+})
+export class ListadoContactos {
+
+}
