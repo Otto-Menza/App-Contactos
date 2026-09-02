91dad26d2955575ef5c6d4d282c73a283aeb6bda
otto-menza <otoniel.jump@gmail.com>
2026-09-02 18:17:35 -0500

    template of contac edit

--- src/app/servicios/contacto-service.ts
+++ src/app/servicios/contacto-service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Contacto } from '../modelo/contacto.modelo';
-import { collection, collectionData, Firestore, orderBy, query, CollectionReference } from '@angular/fire/firestore';
-import { addDoc } from 'firebase/firestore';
+import { collection, collectionData, Firestore, orderBy, query, CollectionReference, docData } from '@angular/fire/firestore';
+import { addDoc, doc } from 'firebase/firestore';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +10,6 @@ import { addDoc } from 'firebase/firestore';
 export class ContactoService {
   contactos: Observable<Contacto[]>;
   private contactosRef: CollectionReference<Contacto>;
-
   constructor(private firestore: Firestore){
     //Realizamos la consulta para obtener el listdo de contactos
     this.contactosRef = collection(this.firestore, 'contactos');
@@ -24,5 +23,10 @@ export class ContactoService {
 
   agregarContacto(contacto: Contacto){
     return addDoc(this.contactosRef, contacto);
+  }
+
+  getContacto(id: string): Observable<Contacto | null>{
+    const contactoRefDoc = doc(this.firestore, `contactos/${id}`);
+    return docData(contactoRefDoc, {idField: 'id'}) as Observable<Contacto>;
   }
 }
